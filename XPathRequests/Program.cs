1ba932f22cc87d69eeb46616b632919fe041095a
Порохин Юрий Максимович <116000343+ExPl0i@users.noreply.github.com>
2024-03-05 12:20:23 +0700

    Закончена лаба 2

--- XPathRequests/Program.cs
+++ XPathRequests/Program.cs
@@ -65,13 +65,99 @@ namespace XpathRequests3
         /// Выводит данные о всех вкладах, сделанных клиентом
         /// </summary>
         /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="fname">Имя</param>
+        /// <param name="sname"><Отчество/param>
+        /// <param name="lname">Фамилия</param>
         static void Query3(XPathNavigator xpNav, string fname, string sname, string lname)
         {
             string title = string.Format("3. Все вклады, сделанные клиентом {0} {1} {2} :", fname, sname, lname);
-            string exprs = string.Format("count(t:bank/t:Client[t:Name/FirstName = {0} and t:Name/SeconName = {1} and t:Name/LastName = {2}]/t:debit)", fname, sname, lname);
+            string exprs = string.Format("count(t:bank/t:Client[t:Name/t:FirstName = '{0}' and t:Name/t:SecondName = '{1}' and t:Name/t:LastName = '{2}']/t:debit)", fname, sname, lname);
             XPathProcess(xpNav, exprs, title);
         }
 
+        /// <summary>
+        /// Выводит данные о кредитах, у которых процентная ставка меньше заданной
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="procent">Процентная ставка</param>
+        static void Query4(XPathNavigator xpNav, double procent)
+        {
+            string title = string.Format("4. Число кредитов по которым процентная ставка меньше {0}:", procent);
+            string exprs = string.Format("count(t:bank/t:Client/t:credit[t:procent<{0}])", procent);
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит данные о кредитах, взятых в заданную дату, у которых сумма больше заданной
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="data">Дата кредита</param>
+        /// <param name="sum">Сумма кредита</param>
+        static void Query5(XPathNavigator xpNav, string data, int sum)
+        {
+            string title = string.Format("5. Кредиты, взятые {0} на сумму более {1}:", data, sum);
+            string exprs = string.Format("count(t:bank/t:Client/t:credit[t:date = '{0}' and t:sum>{1}])", data, sum);
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит данные о среднем размере вклада
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        static void Query6(XPathNavigator xpNav)
+        {
+            string title = string.Format("6. Средний размер вклада:");
+            string exprs = string.Format("sum(t:bank/t:Client/t:debit/t:sum) div count(t:bank/t:Client/t:debit)");
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит данные о клиентах, взявших более одного кредита
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        static void Query7(XPathNavigator xpNav)
+        {
+            string title = string.Format("7. Клиенты, взявшие более одного кредита:");
+            string exprs = string.Format("t:bank/t:Client[count(t:credit) > 1]/t:Name");
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит данные о числе вкладов, размер которых находится между заданными величинами
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="sum1">Первая величина</param>
+        /// <param name="sum2">Вторая величина</param>
+        static void Query8(XPathNavigator xpNav, int sum1, int sum2)
+        {
+            string title = string.Format("8. Число вкладов, размер которых составляет от {0} до {1}:", sum1, sum2);
+            string exprs = string.Format("count(t:bank/t:Client/t:debit[t:sum > {0} and t:sum < {1}])", sum1, sum2);
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит ФИО клиента, которому принадлежит вклад, с указанным кодом
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="code">Код вклада</param>
+        static void Query9(XPathNavigator xpNav, string code)
+        {
+            string title = string.Format("9. ФИО клиента, которому принадлежит вклад с кодом {0}:", code);
+            string exprs = string.Format("t:bank/t:Client[t:debit[@id = '{0}']]/t:Name", code);
+            XPathProcess(xpNav, exprs, title);
+        }
+
+        /// <summary>
+        /// Выводит данные о числе кредитов, предшествующих кредиту с заданным кодом
+        /// </summary>
+        /// <param name="xpNav">Объект XPathNavigator</param>
+        /// <param name="code">Код кредита</param>
+        static void Query10(XPathNavigator xpNav, int code)
+        {
+            string title = string.Format("10. Число кредитов, предшествующих в документе кредиту с кодом {0}:", code);
+            string exprs = string.Format("count(t:bank/t:Client/t:credit[@id < {0}])", code);
+            XPathProcess(xpNav, exprs, title);
+        }
         static void Main(string[] args)
         {
             Console.Title = "XPath-запросы к XML-документу";
@@ -87,6 +173,20 @@ namespace XpathRequests3
             Query2(xpNav);
             Console.WriteLine();
             Query3(xpNav, "Иван", "Петрович", "Сидтиков");
+            Console.WriteLine();
+            Query4(xpNav, 10.0);
+            Console.WriteLine();
+            Query5(xpNav, "01.02.2024", 1000000);
+            Console.WriteLine();
+            Query6(xpNav);
+            Console.WriteLine();
+            Query7(xpNav);
+            Console.WriteLine();
+            Query8(xpNav, 100000, 10000000);
+            Console.WriteLine();
+            Query9(xpNav, "1");
+            Console.WriteLine();
+            Query10(xpNav, 3);
             Console.WriteLine();
 
             Console.Read();
