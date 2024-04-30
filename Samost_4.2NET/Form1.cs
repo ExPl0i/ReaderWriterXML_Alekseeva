ce1f5d1a00759b5161aedea5ef5d683ccd29258f
Порохин Юрий Максимович <116000343+ExPl0i@users.noreply.github.com>
2024-04-30 21:53:50 +0700

    Самостоятельная 4

--- Samost_4.2NET/Form1.cs
+++ Samost_4.2NET/Form1.cs
@@ -27,6 +27,50 @@ namespace Samost_4._2NET
             xmlUri = @"C:\Users\mripo\Source\Repos\ExPl0i\ReaderWriterXML_Alekseeva\XML\Samost_4.xml";
         }
 
+        /// <summary>
+        /// Возвращает индекс цеха в списке worksops по выделенной ячейке
+        /// dataGridWorkshops (при отсутствии объекта возвращает -1)
+        /// </summary>
+        /// <returns>Индекс цеха</returns>
+        private int GetWorkshopIndex()
+        {
+            // Индекс, выводимый при отсутствии выбранного элемента
+            const int incorIndex = -1;
+            // Проверяем число выделенных ячеек в dataGridWorkshops
+            if (dataGridWorkshops.SelectedCells.Count > 0)
+            {
+                int workshopIndex = dataGridWorkshops.SelectedCells[0].RowIndex;
+                if (workshopIndex < workshops.Count)
+                {
+                    return workshopIndex;
+                }
+                else return incorIndex;
+            }
+            else return incorIndex;
+        }
+
+        /// <summary>
+        /// Возвращает индекс станка в списке machines по выделенной
+        /// ячейке dataGridMachines (при отсутствии объекта возвращает -1)
+        /// </summary>
+        /// <param name="workshopIndex"></param>
+        /// <returns></returns>
+        private int GetMachineIndex(int workshopIndex)
+        {
+            const int incorIndex = -1;
+            if (workshopIndex < 0) { return incorIndex; }
+            if (dataGridMachines.SelectedCells.Count > 0)
+            {
+                int machineIndex = dataGridMachines.SelectedCells[0].RowIndex;
+                if (machineIndex < workshops[workshopIndex].Machines.Count)
+                {
+                    return machineIndex;
+                }
+                else return incorIndex;
+            }
+            else return incorIndex;
+        }
+
         private void dataGridWorkshops_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
